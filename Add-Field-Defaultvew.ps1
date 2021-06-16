76f827ac41f80560a0e25fe699867e23dcdb6d50
Ravinder <85952665+Ravieps@users.noreply.github.com>
2021-06-16 19:35:40 +0530

    Update Add-Field-Defaultvew.ps1

--- Add-Field-Defaultvew.ps1
+++ Add-Field-Defaultvew.ps1
@@ -1,6 +1,16 @@
-﻿$web= Get-SPWeb http://app/
+#Get web
+
+$web= Get-SPWeb http://app/
+
+#Get list
 $list = $web.lists["Ravi"]
+
+#Get All items view
 $views = $list.Views["All Items"]
+
+#Add fields to the view
 $views.ViewFields.Add("Name")
 $views.ViewFields.Add("Owner")
+
+#Update the view
 $views.update()
